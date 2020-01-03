bdce57bc0215ade6f3c4d479ca9277918086cf61
贠乾 <yunqian8@live.com>
2020-01-03 18:16:32 +0800

    暂时提交

--- Consul.WebApi.ServiceB/Common/Appsettings.cs
+++ Consul.WebApi.ServiceB/Common/Appsettings.cs
@@ -28,8 +28,6 @@ namespace Consul.WebApi.ServiceB.Common
                .SetBasePath(contentPath)
                .Add(new JsonConfigurationSource { Path = Path, Optional = false, ReloadOnChange = true })//这样的话，可以直接读目录里的json文件，而不是 bin 文件夹下的，所以不用修改复制属性
                .Build();
-
-
         }
 
         /// <summary>
