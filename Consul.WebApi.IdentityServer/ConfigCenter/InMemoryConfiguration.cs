7d20aaa003d489e4ff001d01907f7c9784168e98
贠乾 <yunqian8@live.com>
2020-01-14 12:14:06 +0800

    add .NET Core Microservices Establish authorization and verification services based on IdentityServer test demo

---
+++ Consul.WebApi.IdentityServer/ConfigCenter/InMemoryConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consul.WebApi.IdentityServer.ConfigCenter
+{
+    public static class InMemoryConfiguration
+    {
+        // 这个 Authorization Server 保护了哪些 API （资源）
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                    new ApiResource("consul.webapi.servicea", "Consul.WebApi.ServiceA"),
+                    new ApiResource("consul.webapi.serviceb", "Consul.WebApi.ServiceB")
+            };
+        }
+        // 哪些客户端 Client（应用） 可以使用这个 Authorization Server
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                    new Client
+                    {
+                        ClientId = "serviceA",//定义客户端 Id
+                        ClientSecrets = new [] { new Secret("secret".Sha256()) },//Client用来获取token
+                        AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,//这里使用的是通过用户名密码和ClientCredentials来换取token的方式. ClientCredentials允许Client只使用ClientSecrets来获取token. 这比较适合那种没有用户参与的api动作
+                        AllowedScopes = new [] { "consul.webapi.servicea" }// 允许访问的 API 资源
+                    },
+                    new Client
+                    {
+                        ClientId = "serviceB",//定义客户端 Id
+                        ClientSecrets = new [] { new Secret("secret".Sha256()) },//Client用来获取token
+                        AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,//这里使用的是通过用户名密码和ClientCredentials来换取token的方式. ClientCredentials允许Client只使用ClientSecrets来获取token. 这比较适合那种没有用户参与的api动作
+                        AllowedScopes = new [] { "consul.webapi.serviceb" }// 允许访问的 API 资源
+                    }
+                };
+        }
+        // 指定可以使用 Authorization Server 授权的 Users（用户）
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[]
+            {
+                    new TestUser
+                    {
+                        SubjectId = "1",
+                        Username = "yunqian",
+                        Password = "qwer1234!"
+                    }
+            };
+        }
+    }
+}
---
+++ Consul.WebApi.IdentityServer/Startup.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Consul.WebApi.IdentityServer.ConfigCenter;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Consul.WebApi.IdentityServer
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            #region ²âÊÔIdentityServer4
+            var builder = services.AddIdentityServer(options =>
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+                })
+                // in-memory, code config
+                .AddTestUsers(InMemoryConfiguration.Users().ToList())
+                .AddInMemoryApiResources(InMemoryConfiguration.GetApiResources())
+                .AddInMemoryClients(InMemoryConfiguration.GetClients());
+
+
+            builder.AddDeveloperSigningCredential();
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+            #endregion
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            //open ids4 authorization 
+            app.UseIdentityServer();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
