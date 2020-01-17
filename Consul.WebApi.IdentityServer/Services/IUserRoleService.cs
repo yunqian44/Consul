264249c72acd17719f1bce201ef748eff2f92ffd
贠乾 <yunqian8@live.com>
2020-01-17 15:30:05 +0800

    add user role relationship service

---
+++ Consul.WebApi.IdentityServer/Services/IUserRoleService.cs
@@ -0,0 +1,19 @@
+﻿using Consul.WebApi.IdentityServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consul.WebApi.IdentityServer.Services
+{
+    public interface IUserRoleService
+    {
+        #region 01,get userrole listing+Task<List<UserRoleModel>> QueryUserRoleList();
+        /// <summary>
+        /// get userrole listing
+        /// </summary>
+        /// <returns></returns>
+        Task<List<UserRoleModel>> QueryUserRoleList(); 
+        #endregion
+    }
+}
---
+++ Consul.WebApi.IdentityServer/Services/UserRoleService.cs
@@ -0,0 +1,23 @@
+﻿using Consul.WebApi.IdentityServer.Helper;
+using Consul.WebApi.IdentityServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consul.WebApi.IdentityServer.Services
+{
+    public class UserRoleService : IUserRoleService
+    {
+        #region 01,get userrole listing+async Task<List<UserRoleModel>> QueryUserRoleList()
+        /// <summary>
+        /// get userrole listing
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<UserRoleModel>> QueryUserRoleList()
+        {
+            return await Task.Run(() => { return JsonHelper.ParseFormByJson<List<UserRoleModel>>(GetTableData.GetData(nameof(UserRoleModel))); });
+        } 
+        #endregion
+    }
+}
