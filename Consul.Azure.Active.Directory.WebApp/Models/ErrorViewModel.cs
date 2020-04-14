98391941c763539dd483951eafcdd16bed40acd9
yunqian44 <yunqian8@live.com>
2020-04-14 19:03:24 +0800

    新增WebApp Test Project

---
+++ Consul.Azure.Active.Directory.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Consul.Azure.Active.Directory.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
