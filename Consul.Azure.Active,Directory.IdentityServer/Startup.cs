eb1fb335c1830e25a444fd0c35a014506849e168
yunqian44 <yunqian8@live.com>
2020-04-14 19:03:47 +0800

    Init IdentityServer Project

--- Consul.Azure.Active,Directory.IdentityServer/Startup.cs
+++ Consul.Azure.Active,Directory.IdentityServer/Startup.cs
@@ -22,6 +22,8 @@ namespace Consul.Azure.Active_Directory.IdentityServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddAuthentication("").AddAzureADBearer()
+
             services.AddRazorPages();
         }
 
